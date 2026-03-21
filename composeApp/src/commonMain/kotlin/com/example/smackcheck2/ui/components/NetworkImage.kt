6b68ea53e27210d47faa4fabe4b434c6edda436e
Poojitha Gobburu <poojithagobburu@gmail.com>
2026-03-21 20:19:52 +0530

    Fix missing Icon import in NetworkImage.kt for iOS build

--- composeApp/src/commonMain/kotlin/com/example/smackcheck2/ui/components/NetworkImage.kt
+++ composeApp/src/commonMain/kotlin/com/example/smackcheck2/ui/components/NetworkImage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Restaurant
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
